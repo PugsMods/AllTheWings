5974ba0279c48d92f875c322ea247b3acb370930
PugzAreCute <me@pugzarecute.com>
2021-07-21 18:09:34 +0530

    Try to test something but broke everything

--- src/main/java/com/pugzarecute/allthewings/AllTheWings.java
+++ src/main/java/com/pugzarecute/allthewings/AllTheWings.java
@@ -38,6 +38,7 @@ public class AllTheWings {
     @OnlyIn(Dist.CLIENT)
     private void elytraLayer()
     {
+        System.out.println("textures/entity/"+Init.DIRT_ELYTRA.get().getRegistryName().getPath()+".png");
         Minecraft.getInstance().getEntityRenderDispatcher().getSkinMap().values().forEach(player -> player.addLayer(new ElytraLayerRenderManager(player,Init.DIRT_ELYTRA)));
     }
 }
--- src/main/java/com/pugzarecute/allthewings/ElytraLayerRenderManager.java
+++ src/main/java/com/pugzarecute/allthewings/ElytraLayerRenderManager.java
@@ -14,7 +14,7 @@ import javax.annotation.Nonnull;
 public class ElytraLayerRenderManager extends net.minecraft.client.renderer.entity.layers.ElytraLayer<AbstractClientPlayerEntity, PlayerModel<AbstractClientPlayerEntity>>
 {
     private static RegistryObject<Item> ELYTRA_IN = null;
-    private  final ResourceLocation TEXTURE = new ResourceLocation("WILL BE FILLED");
+    private  final ResourceLocation TEXTURE = new ResourceLocation("textures/entity/"+ELYTRA_IN.get().getRegistryName()+".png");
 
     public ElytraLayerRenderManager(IEntityRenderer<AbstractClientPlayerEntity, PlayerModel<AbstractClientPlayerEntity>> rendererIn, @Nonnull RegistryObject<Item> ELYTRA_IN)
     {
