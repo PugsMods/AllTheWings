e430877a65ce35bb4151580e3da7f2b198437e81
PugzAreCute <me@pugzarecute.com>
2021-07-21 13:26:23 +0530

    Lots of stuff

--- src/main/java/com/pugzarecute/allthewings/AllTheWings.java
+++ src/main/java/com/pugzarecute/allthewings/AllTheWings.java
@@ -2,8 +2,12 @@ package com.pugzarecute.allthewings;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -23,5 +27,17 @@ import java.util.stream.Collectors;
 public class AllTheWings {
     public static final String MODID = "allthewings";
     public AllTheWings() {
+        final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        Init.ITEMS.register(bus);
+        bus.addListener(this::client);
+    }
+    private void client(FMLClientSetupEvent event)
+    {
+        elytraLayer();
+    }
+    @OnlyIn(Dist.CLIENT)
+    private void elytraLayer()
+    {
+        Minecraft.getInstance().getEntityRenderDispatcher().getSkinMap().values().forEach(player -> player.addLayer(new ElytraLayer(player)));
     }
 }
--- src/main/java/com/pugzarecute/allthewings/Init.java
+++ src/main/java/com/pugzarecute/allthewings/Init.java
@@ -7,6 +7,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class Init {
-    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AllTheWings.MODID);
-    private static final RegistryObject<Item> DIRT_ELYTRA = ITEMS.register("dirt_elytra",() -> new ElytraBase(new Item.Properties().maxDamage(10).group(ItemGroup.MISC)));
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AllTheWings.MODID);
+    private static final RegistryObject<Item> DIRT_ELYTRA = ITEMS.register("dirt_elytra",() -> new ElytraBase(new Item.Properties().maxDamage(100).group(ItemGroup.MISC)));
 }
