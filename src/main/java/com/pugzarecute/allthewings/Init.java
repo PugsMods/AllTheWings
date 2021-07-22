a05a83813b453be72673321db133393d445c1b5e
PugzAreCute <me@pugzarecute.com>
2021-07-22 15:18:12 +0530

    Add more dirt elytras

--- src/main/java/com/pugzarecute/allthewings/Init.java
+++ src/main/java/com/pugzarecute/allthewings/Init.java
@@ -9,4 +9,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class Init {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AllTheWings.MODID);
     public static final RegistryObject<Item> DIRT_ELYTRA = ITEMS.register("dirt_elytra",() -> new ElytraBase(new Item.Properties().durability(100).tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> GRASS_ELYTRA = ITEMS.register("grass_elytra",() -> new ElytraBase(new Item.Properties().durability(100).tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> PODZOL_ELYTRA = ITEMS.register("podzol_elytra",() -> new ElytraBase(new Item.Properties().durability(100).tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> PATH_ELYTRA = ITEMS.register("path_elytra",() -> new ElytraBase(new Item.Properties().durability(100).tab(ItemGroup.TAB_MISC)));
 }
