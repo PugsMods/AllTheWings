20a335dd501c1c90ecdacb32868a7d5a46fa275a
PugzAreCute <me@pugzarecute.com>
2021-07-22 15:01:47 +0530

    Import cleanup

--- src/main/java/com/pugzarecute/allthewings/AllTheWings.java
+++ src/main/java/com/pugzarecute/allthewings/AllTheWings.java
@@ -1,27 +1,12 @@
 package com.pugzarecute.allthewings;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.lwjgl.system.CallbackI;
-
-import java.util.stream.Collectors;
 
 @Mod("allthewings")
 public class AllTheWings {
--- src/main/java/com/pugzarecute/allthewings/ElytraLayerRenderManager.java
+++ src/main/java/com/pugzarecute/allthewings/ElytraLayerRenderManager.java
@@ -7,9 +7,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.RegistryObject;
-import org.lwjgl.system.CallbackI;
-
-import javax.annotation.Nonnull;
 
 public class ElytraLayerRenderManager extends net.minecraft.client.renderer.entity.layers.ElytraLayer<AbstractClientPlayerEntity, PlayerModel<AbstractClientPlayerEntity>>
 {
