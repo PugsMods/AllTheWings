74d395d87463c476f4acf297d47d3d22ceb4bb54
PugzAreCute <me@pugzarecute.com>
2021-07-23 21:08:29 +0530

    Make dyed elytras weaker

--- src/main/java/com/pugzarecute/allthewings/Init.java
+++ src/main/java/com/pugzarecute/allthewings/Init.java
@@ -8,7 +8,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class Init {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AllTheWings.MODID);
-    public static final RegistryObject<Item> ORANGE_TO_RED_ELYTRA = ITEMS.register("orange_to_red_elytra",() -> new ElytraBase(new Item.Properties().durability(432).tab(ItemGroup.TAB_MISC)));
-    public static final RegistryObject<Item> YOUR_BEING_WATCHED_ELYTRA = ITEMS.register("your_being_watched_elytra",() -> new ElytraBase(new Item.Properties().durability(432).tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> ORANGE_TO_RED_ELYTRA = ITEMS.register("orange_to_red_elytra",() -> new ElytraBase(new Item.Properties().durability(425).tab(ItemGroup.TAB_MISC)));
+    public static final RegistryObject<Item> YOUR_BEING_WATCHED_ELYTRA = ITEMS.register("your_being_watched_elytra",() -> new ElytraBase(new Item.Properties().durability(425).tab(ItemGroup.TAB_MISC)));
 
 }
